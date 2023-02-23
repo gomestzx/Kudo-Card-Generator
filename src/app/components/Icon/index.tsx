1545ee320aa8735a7a3008e51d65a5812667a8c9
gomestzx <felipegomes2009@gmail.com>
2023-02-23 11:16:53 -0300

    feat: change png to svg

--- src/app/components/Icon/index.tsx
+++ src/app/components/Icon/index.tsx
@@ -4,17 +4,17 @@ import Image from 'next/image';
 export function Icon(param: string) {
   switch (param) {
     case 'Totally awesome':
-      return <Image src='/images/1.png' width={200} height={200} alt='icon' />;
+      return <Image src='/images/1.svg' width={200} height={200} alt='icon' />;
     case 'Thank you':
-      return <Image src='/images/2.png' width={200} height={200} alt='icon' />;
+      return <Image src='/images/2.svg' width={200} height={200} alt='icon' />;
     case 'Very happy':
-      return <Image src='/images/3.png' width={200} height={200} alt='icon' />;
+      return <Image src='/images/3.svg' width={200} height={200} alt='icon' />;
     case 'Well done':
-      return <Image src='/images/4.png' width={200} height={200} alt='icon' />;
+      return <Image src='/images/4.svg' width={200} height={200} alt='icon' />;
     case 'Great job':
-      return <Image src='/images/5.png' width={200} height={200} alt='icon' />;
+      return <Image src='/images/5.svg' width={200} height={200} alt='icon' />;
     case 'Congratulations':
-      return <Image src='/images/6.png' width={200} height={200} alt='icon' />;
+      return <Image src='/images/6.svg' width={200} height={200} alt='icon' />;
   }
 }
 
