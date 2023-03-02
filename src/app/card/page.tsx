acd96e2335c3a1fdb006b09939f2954986afdeae
gomestzx <felipegomes2009@gmail.com>
2023-03-02 10:38:56 -0300

    feat: add useMemo to card page

--- src/app/card/page.tsx
+++ src/app/card/page.tsx
@@ -2,13 +2,13 @@
 
 import Preview from '../components/Preview';
 import { useSearchParams } from 'next/navigation';
+import { useMemo } from 'react';
 
 export default function Card() {
   const searchParams = useSearchParams();
-  const color = searchParams.get('color');
-  const title = searchParams.get('title');
-  const text = searchParams.get('text');
-
+  const color = useMemo(() => searchParams.get('color'), [searchParams]);
+  const title = useMemo(() => searchParams.get('title'), [searchParams]);
+  const text = useMemo(() => searchParams.get('text'), [searchParams]);
 
   return (
     <div className='app'>
